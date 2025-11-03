4a8d9282451cbcf213f0cf30836ace3e84b070fc
David Shaw <dshaw11@utk.edu>
2025-11-03 15:24:00 -0500

    setting up the test window

--- src/tasklist.cpp
+++ src/tasklist.cpp
@@ -1,3 +1,34 @@
 #include "task.h"
 #include <iostream>
 #include <string>
+
+int main() {
+
+    char choice = 'i';
+
+    do
+    {
+        cout << "Welcome to the Task Manager!\n\n";
+        cout << "Please select an action:\n";
+        cout << "\t a: add a new task\n";
+        cout << "\t e: edit a new task\n";
+        cout << "\t d: delete a task\n";
+        cout << "\t s: sort task list (by something)\n";
+        cin >> choice;
+        cin.ignore(); //ignore trailing \n
+        executeAction(choice);
+    } while (choice != 'q');
+
+    return 0;
+}
+
+void executeAction(char c)
+{
+    switch (c)
+    {
+        case 'q':
+            cout << "Goodbye";
+            break;
+        default: cout << c << " is an invalid input!\n";
+    }
+}
